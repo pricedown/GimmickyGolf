71c20362d67ded336e944ac1cdc2a3ebb671f3d8
FeatherMustar <82784762+FeatherMustar@users.noreply.github.com>
2023-11-07 11:52:09 -0500

    stroke count

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Hole : MonoBehaviour
 {
     public float timeDiff;
     private float storedTime;
     private bool hasCompleted = false;
+    private GameObject player;
     public GameObject holeCompleteText;
+    public TextMeshProUGUI strokeText;
 
     private void Start()
     {
@@ -16,18 +19,24 @@ public class Hole : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        storedTime = Time.time; // Get time the ball gets in the hole
+        if (collision.CompareTag("Player"))
+        {
+            storedTime = Time.time; // Get time the ball gets in the hole
+        }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(Time.time - storedTime > timeDiff && !hasCompleted) // Check if the ball has been in the hole for long enough
+        if(Time.time - storedTime > timeDiff && !hasCompleted && collision.CompareTag("Player")) // Check if the ball has been in the hole for long enough
         {
+            player = collision.gameObject;
             HoleComplete();
         }
     }
     private void HoleComplete()
     {
         holeCompleteText.SetActive(true); // Show the text saying the hole has been completed
+        int strokes = player.GetComponent<BallMovement>().strokeCount;
+        strokeText.text = "Strokes: " + strokes;
         hasCompleted = true;
         Time.timeScale = 0f; // Stop the scene
     }
