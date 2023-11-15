30fce051ff8ee90af5b5fe35ade57b909dc3bbde
joseph.isaacs <joseph.isaacs@mymail.champlain.edu>
2023-11-15 14:52:59 -0500

    untested trajectory code, signoff

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -190,12 +190,21 @@ public class BallMovement : MonoBehaviour
             const int margin = 2;
             if (i + margin >= trajectoryIndicator.positionCount)
                 continue;
+            
+            // ?only check if it's a certain distance away
             if (Vector3.Distance(transform.position, newPoint) > 2*collider.radius)
             {
+                /*
                 if (Physics2D.OverlapCircle(newPoint, collider.radius - 0.3f))
                 {
                     trajectoryIndicator.positionCount = i+margin;
-                }
+                }*/
+                // TODO: add filter for triggers in OverlapArea
+                Vector2 p1 = new Vector2(points.Last().x, points.Last().y + (collider.radius * 0.5f));
+                Vector2 p2 = new Vector2(newPoint.x, newPoint.y - (collider.radius * 0.5f));
+                
+                if (Physics2D.OverlapArea(p1, p2))
+                    trajectoryIndicator.positionCount = i+margin;
             }
         }
         return points.ToArray();
