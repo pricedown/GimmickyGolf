c4df753f2e54ae8426a46c4622362f57d74ccdf1
Caleb Carreon <caleb.carreon@mymail.champlain.edu>
2023-11-29 12:48:34 -0500

    scoresheet

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -11,6 +11,7 @@ public class LevelManager : MonoBehaviour
     public TextMeshProUGUI strokeText;
     public TextMeshProUGUI currentStrokesText;
     public TextMeshProUGUI allStrokesText;
+    public GameObject allStrokes;
     public GameObject pausedScreen;
 
     public void Start()
@@ -31,17 +32,20 @@ public class LevelManager : MonoBehaviour
         holeCompleteText.SetActive(false);
         pausedScreen.SetActive(false);
         currentStrokesText.gameObject.SetActive(false);
+        allStrokes.SetActive(false);
         Time.timeScale = 1f;
     }
     public void HoleComplete(GameObject player)
     {
         holeCompleteText.SetActive(true); // Show the text saying the hole has been completed
+        allStrokes.SetActive(true);
         int strokes = player.GetComponent<BallMovement>().strokeCount;
-        if (PlayerPrefs.GetInt(SceneManager.GetActiveScene().name + "Best", strokes) >= strokes)
+        if (PlayerPrefs.GetInt(SceneManager.GetActiveScene().name + "Best", strokes) >= strokes || PlayerPrefs.GetInt(SceneManager.GetActiveScene().name + "Best", strokes) == 0)
         {
             PlayerPrefs.SetInt(SceneManager.GetActiveScene().name + "Best", strokes);
         }
         strokeText.text = "Strokes: " + strokes + "\t\tBest: " + PlayerPrefs.GetInt(SceneManager.GetActiveScene().name + "Best", strokes);
+        SetScoreSheet();
         Time.timeScale = 0f; // Stop the scene
     }
     public void SetCurrentStrokes(int currentStrokes)
@@ -54,29 +58,47 @@ public class LevelManager : MonoBehaviour
     }
     public void PauseUnpause()
     {
-        if(Time.timeScale == 0f)
+        if(Time.timeScale == 0f && !holeCompleteText.activeSelf)
         {
             Time.timeScale = 1f;
             pausedScreen.SetActive(false);
+            allStrokes.SetActive(false);
         }
         else if (!holeCompleteText.activeSelf)
         {
             Time.timeScale = 0f;
-            string level = "W1L1";
-            allStrokesText.text = "";
-            while (SceneUtility.GetBuildIndexByScenePath(level) != -1)
-            {
-                if (PlayerPrefs.GetInt(SceneManager.GetActiveScene().name + "Best", 0) != 0)
-                {
-                    allStrokesText.text += PlayerPrefs.GetInt(level + "Best", 0) + "\n";
-                }
-                else
-                {
-                    allStrokesText.text += "X\n";
-                }
-                level = GetNextLevel(level);
-            }
+            SetScoreSheet();
+            allStrokes.SetActive(true);
             pausedScreen.SetActive(true);
+        }
+    }
+    public void SetScoreSheet()
+    {
+        string level = "W1L1";
+        allStrokesText.text = "";
+        while (SceneUtility.GetBuildIndexByScenePath(level) != -1)
+        {
+            if (PlayerPrefs.GetInt(level + "Best", 0) != 0)
+            {
+                allStrokesText.text += level + ": " + PlayerPrefs.GetInt(level + "Best", 0) + "\n";
+            }
+            else
+            {
+                allStrokesText.text += level + ": " + "X\n";
+            }
+            level = GetNextLevel(level);
+        }
+    }
+    public void ResetScores()
+    {
+        string level = "W1L1";
+        while (SceneUtility.GetBuildIndexByScenePath(level) != -1)
+        {
+            if (PlayerPrefs.GetInt(level + "Best", 0) != 0)
+            {
+                PlayerPrefs.SetInt(level + "Best", 0);
+            }
+            level = GetNextLevel(level);
         }
     }
     public string GetNextLevel(string level)
