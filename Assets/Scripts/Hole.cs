370d29fab314903632ae0764c79e17911938a096
Autuwumn <vargishjj@gmail.com>
2023-12-12 12:49:40 -0500

    added a flag

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -9,11 +9,13 @@ public class Hole : MonoBehaviour
     public float timeDiff;
     private float storedTime;
     private bool hasCompleted = false;
+    private bool flagRising = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            flagRising = true;
             storedTime = Time.time; // Get time the ball gets in the hole
         }
     }
@@ -33,5 +35,22 @@ public class Hole : MonoBehaviour
             hasCompleted = true;
         }
     }
-
+    private void Update()
+    {
+        if(flagRising)
+        {
+            var flag = transform.GetChild(2).GetChild(0).transform;
+            flag.transform.localPosition += new Vector3(0, Time.deltaTime, 0);
+            if (flag.transform.localPosition.y >= 0.39f)
+            {
+                flagRising = false;
+                flag.transform.localPosition = new Vector3(2.3f, 0.39f, 0);
+            }
+        }
+    }
+    public void FlagRaise()
+    {
+        flagRising = true;
+        var flag = transform.GetChild(2).GetChild(0).transform;
+    }
 }
